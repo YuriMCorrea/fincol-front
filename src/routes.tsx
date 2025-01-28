f8ee7c5abf0aa56ff1b3f9b0e9367e849338d8c9
YuriMCorrea <yurimcorrea@gmail.com>
2025-01-28 12:46:38 -0300

    Added routes with base layout and nested children

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,61 @@
-import { createBrowserRouter } from 'react-router-dom'
+import {
+    createBrowserRouter,
+} from "react-router-dom";
+import Layout from "./pages/layout";
+import { Home } from "./pages/app/home";
+import { ErrorPage } from "./pages/app/error-page.tsx";
 
-import { Dashboard } from './pages/app/dashboard'
-import { SignIn } from './pages/auth/sign-in'
 
 export const router = createBrowserRouter([
     {
-        path: '/',
-        element: <Dashboard />,
+        path: "/",
+        element: <Layout />,
+        children: [
+            {
+                index: true,
+                element: <Home />,
+            },
+            {
+                path: "about",
+                // Single route in lazy file
+                lazy: async () => {
+                    let {About} = await import("./pages/app/about.tsx");
+                    return {Component: About};
+                }
+            },
+            {
+                path: "dashboard",
+                async lazy() {
+                    // Multiple routes in lazy file
+                    let { DashboardLayout } = await import("./pages/app/dashboard");
+                    return { Component: DashboardLayout };
+                },
+                children: [
+                    {
+                        index: true,
+                        async lazy() {
+                            let { DashboardIndex } = await import("./pages/app/dashboard");
+                            return { Component: DashboardIndex };
+                        },
+                    },
+                    {
+                        path: "messages",
+                        async lazy() {
+                            let { dashboardMessagesLoader, DashboardMessages } = await import(
+                                "./pages/app/dashboard.tsx"
+                                );
+                            return {
+                                loader: dashboardMessagesLoader,
+                                Component: DashboardMessages,
+                            };
+                        },
+                    },
+                ],
+            },
+            {
+                path: "*",
+                element: <ErrorPage />,
+            },
+        ],
     },
-    {
-        path: '/sign-in',
-        element: <SignIn />,
-    },
-])
+]);
---
+++ src/routes/about.tsx
@@ -0,0 +1,14 @@
+import { useLoaderData } from "react-router-dom";
+
+export async function clientLoader() {
+    return {
+        title: "About",
+    };
+}
+
+export default function About() {
+    let data = useLoaderData();
+    return <div>{data.title}</div>;
+}
+
+// clientAction, ErrorBoundary, etc.
