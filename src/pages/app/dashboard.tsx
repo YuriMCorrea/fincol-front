bf8cc488aa3d4fc115877856e11b4be6782d4c50
YuriMCorrea <yurimcorrea@gmail.com>
2025-01-27 22:47:42 -0300

    Installing react Router Dom

---
+++ src/pages/app/dashboard.tsx
@@ -0,0 +1,12 @@
+import {Button} from "@/components/ui/button.tsx";
+
+export function Dashboard() {
+    return (
+    <>
+        <h1>Dashboard</h1>
+        <div className="flex justify-center items-center h-screen">
+            <Button>Enviar</Button>
+        </div>
+    </>
+    );
+}
