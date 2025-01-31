4bac3d010bedbdce06bfb9b2b6e25b05908393f3
YuriMCorrea <yurimcorrea@gmail.com>
2025-01-31 20:43:23 -0300

    changing root path

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,7 +7,7 @@ import { ErrorPage } from "./pages/app/error-page.tsx";
 
 export const router = createBrowserRouter([
     {
-        path: "/",
+        path: "/fincol-front",
         element: <Base />,
         children: [
             {
